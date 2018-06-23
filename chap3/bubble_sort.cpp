31419a855cba4defde639bb35f7dd2687f74aa4d
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2018-06-23 13:19:16 +0900

    update git config

--- chap3/bubble_sort.cpp
+++ chap3/bubble_sort.cpp
@@ -4,14 +4,13 @@ using namespace std;
 /* Swap and count its number */
 int bubbleSort(int A[], int n) {
   int sw = 0;
-  bool flg = 1;
   for (int i=0; i<n-1; i++) {
     for (int j=n-1; j>i; j--) {
       if (A[j-1] > A[j]) {
         swap(A[j-1], A[j]);
         sw ++;
       }
-    }  
+    }
   }
   return sw;
 }
