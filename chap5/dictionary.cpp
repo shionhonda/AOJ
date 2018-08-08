650cd3979fe9bb1fc67de9125efad718ce4cfb8e
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2018-08-08 21:56:43 +0900

    dictionary

---
+++ chap5/dictionary.cpp
@@ -0,0 +1,88 @@
+#include<cstring>
+#include<cstdio>
+using namespace std;
+
+#define M 1000000
+#define L 13
+char H[M][L];
+
+int getChar(char ch) {
+  if (ch=='A') {
+    return 1;
+  } else if (ch=='C') {
+    return 2;
+  } else if (ch=='G') {
+    return 3;
+  } else if (ch=='T') {
+    return 4;
+  } else {
+    return 0;
+  }
+}
+
+int getKey(char str[]) {
+  int sum=0, p=1;
+  for (int i=0; i<strlen(str); i++) {
+    sum += p*getChar(str[i]);
+    p *= 5;
+  }
+  return sum;
+}
+
+int h1(int key) {
+  return key%M;
+}
+
+int h2(int key) {
+  return 1+key%(M-1);
+}
+
+int find(char str[]) {
+  int key = getKey(str);
+  for (int i=0; ; i++) {
+    int h = (h1(key) + i*h2(key))%M;
+    if (strcmp(H[h], str)==0) {
+      return 1;
+    } else if (strlen(H[h])==0) {
+      return 0;
+    }
+  }
+  return 0;
+}
+
+void insert(char str[]) {
+  int h;
+  int key = getKey(str);
+  for (int i=0; ; i++) {
+    h = (h1(key) + i*h2(key))%M;
+    if (strcmp(H[h], str)==0) {
+      break;
+    } else if (strlen(H[h])==0) {
+      strcpy(H[h], str);
+      break;
+    }
+  }
+}
+
+int main() {
+  int n;
+  char str[L], com[9];
+  for (int i=0; i<M; i++) {
+    H[i][0] = '\0';
+  }
+  scanf("%d", &n);
+  for (int i=0; i<n; i++) {
+    scanf("%s %s", com, str);
+    if (com[0]=='i') {
+      insert(str);
+    } else {
+      if (find(str)) {
+        printf("yes\n");
+      } else {
+        printf("no\n");
+      }
+    }
+  }
+
+  return 0;
+}
