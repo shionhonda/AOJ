b4c532f7ecf22dce059d67b4c5f87391c412cfbb
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2018-10-20 11:22:09 +0900

    Binary search tree II

---
+++ chap9/binary_search_tree_2.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct Node {
+  int key;
+  Node *parent, *left, *right;
+};
+
+Node *root, *NIL;
+
+void insert(int k) {
+  Node *y = NIL;
+  Node *x = root;
+  Node *z;
+  z = (Node*)malloc(sizeof(Node));
+  z->key = k;
+  z->left = NIL;
+  z->right = NIL;
+
+  while (x!=NIL) {
+    y = x;
+    if (z->key < x->key) {
+      x = x->left;
+    } else {
+      x = x->right;
+    }
+  }
+  z->parent = y;
+  if (y==NIL) {
+    root = z;
+  } else {
+    if (z->key < y->key) {
+      y->left = z;
+    } else {
+      y->right = z;
+    }
+  }
+}
+
+void preorder(Node *v) {
+  if (v==NIL) {
+    return;
+  }
+  cout << " " << v->key;
+  preorder(v->left);
+  preorder(v->right);
+}
+
+void inorder(Node *v) {
+  if (v==NIL) {
+    return;
+  }
+  inorder(v->left);
+  cout << " " << v->key;
+  inorder(v->right);
+}
+
+void find(Node *v, int x) {
+  if (v==NIL) {
+    cout << "no" << endl;
+    return;
+  } else if (v->key==x) {
+    cout << "yes" << endl;
+    return;
+  }
+  if (v->key < x) {
+    find(v->right, x);
+  } else{
+    find(v->left, x);
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int x;
+  string com;
+  for (int i=0; i<n; i++) {
+    cin >> com;
+    if (com=="insert") {
+      cin >> x;
+      insert(x);
+    } else if (com=="print") {
+      inorder(root);
+      cout << endl;
+      preorder(root);
+      cout << endl;
+    } else if (com=="find") {
+      cin >> x;
+      find(root, x);
+    }
+  }
+  return 0;
+}
